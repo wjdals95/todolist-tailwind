b4da73c899608fa204d4d1f37a3d0c810e77b696
wjdals95 <wjdals992@naver.com>
2024-04-09 12:17:53 +0900

    finish

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,4 +18,25 @@
       });
     });
 })();
-// endL Navbar
+// end: Navbar
+
+// start: Service
+(function () {
+  document.querySelectorAll("[data-service-for]").forEach(function (toggle) {
+    toggle.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(
+        '[data-service-value="' + toggle.dataset.serviceFor + '"]'
+      );
+      document.querySelectorAll("[data-service-for]").forEach(function (i) {
+        i.classList.remove("active");
+      });
+      document.querySelectorAll("[data-service-value]").forEach(function (i) {
+        i.classList.remove("active");
+      });
+      toggle.classList.add("active");
+      target.classList.add("active");
+    });
+  });
+})();
+// end: Service
